46160063924ab48d2635e37c3ef6ea55ff44fe06
Caleb Froese <cfroese@nextfaze.com>
2018-02-26 19:41:48 +1030

    Add config to handle padding, quotes, replacing, and translate pipe name

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,10 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'extension.generateTranslationString',
     async () => {
+      const settings = vscode.workspace.getConfiguration(
+        'ngx-translate-quickcreate',
+      );
+
       // Get the active editor window
       const editor = vscode.window.activeTextEditor;
       if (!editor) {
@@ -25,11 +29,18 @@ export function activate(context: vscode.ExtensionContext) {
       const value = `"${key}": "${selectedText}"`;
       // Copy the translation json to the clipboard
       copypaste.copy(value);
-      // Replace the selection text with the translated key
-      const translation = `{{ ${key} | translate }}`;
-      editor.edit(builder => {
-        builder.replace(editor.selection, translation);
-      });
+
+      if (settings.get('replaceOnTranslate')) {
+        // Replace the selection text with the translated key
+        const padding = settings.padding ? ' ' : '';
+        const quote = settings.quote;
+        const translation = `{{${padding}${quote}${key}${quote} | ${
+          settings.translatePipeName
+        }${padding}}}`;
+        editor.edit(builder => {
+          builder.replace(editor.selection, translation);
+        });
+      }
     },
   );
   context.subscriptions.push(disposable);
