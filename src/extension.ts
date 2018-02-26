9f7a8b375941e364f71c0ef380090c10f13cf108
Caleb Froese <cfroese@nextfaze.com>
2018-02-26 20:13:47 +1030

    Move the extension functoin into lib folder"

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,11 @@
-'use strict';
-
 import * as vscode from 'vscode';
-import * as copypaste from 'copy-paste';
+
+import { generateTranslationString } from './lib';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'extension.generateTranslationString',
-    async () => {
-      const settings = vscode.workspace.getConfiguration(
-        'ngx-translate-quickcreate',
-      );
-
-      // Get the active editor window
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        vscode.window.showWarningMessage('Select text to translate');
-        return;
-      }
-      // Fetch the selected text
-      const selectedText = editor.document.getText(editor.selection);
-      if (!selectedText) {
-        vscode.window.showInformationMessage('Select text to translate');
-        return;
-      }
-      let input =
-        (await vscode.window.showInputBox({
-          prompt:
-            'Enter a key for this translation or leave blank to use value',
-          placeHolder:
-            'e.g. "hello world" will generate a key named "HELLO_WORLD"',
-        })) || selectedText;
-      const key = input.toUpperCase().replace(' ', '_');
-      // Generate a json key/value pair
-      const value = `"${key}": "${selectedText}"`;
-      // Copy the translation json to the clipboard
-      copypaste.copy(value);
-
-      if (settings.get('replaceOnTranslate')) {
-        // Replace the selection text with the translated key
-        const padding = settings.padding ? ' ' : '';
-        const quote = settings.quote;
-        const translation = `{{${padding}${quote}${key}${quote} | ${
-          settings.translatePipeName
-        }${padding}}}`;
-        editor.edit(builder => {
-          builder.replace(editor.selection, translation);
-        });
-      }
-    },
+    generateTranslationString,
   );
   context.subscriptions.push(disposable);
 }
---
+++ src/lib/generateTranslationString.ts
@@ -0,0 +1,43 @@
+import * as copypaste from 'copy-paste';
+import * as vscode from 'vscode';
+
+export async function generateTranslationString() {
+  const settings = vscode.workspace.getConfiguration(
+    'ngx-translate-quickcreate',
+  );
+
+  // Get the active editor window
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    vscode.window.showWarningMessage('Select text to translate');
+    return;
+  }
+  // Fetch the selected text
+  const selectedText = editor.document.getText(editor.selection);
+  if (!selectedText) {
+    vscode.window.showInformationMessage('Select text to translate');
+    return;
+  }
+  let input =
+    (await vscode.window.showInputBox({
+      prompt: 'Enter a key for this translation or leave blank to use value',
+      placeHolder: 'e.g. "hello world" will generate a key named "HELLO_WORLD"',
+    })) || selectedText;
+  const key = input.toUpperCase().replace(' ', '_');
+  // Generate a json key/value pair
+  const value = `"${key}": "${selectedText}"`;
+  // Copy the translation json to the clipboard
+  copypaste.copy(value);
+
+  if (settings.get('replaceOnTranslate')) {
+    // Replace the selection text with the translated key
+    const padding = settings.padding ? ' ' : '';
+    const quote = settings.quote;
+    const translation = `{{${padding}${quote}${key}${quote} | ${
+      settings.translatePipeName
+    }${padding}}}`;
+    editor.edit(builder => {
+      builder.replace(editor.selection, translation);
+    });
+  }
+}
