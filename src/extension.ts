409faaa473c0340534f91a6f27cd96b7a1a3157e
Ahmed Al-Dolabi <ahmed_dolabi@hotmail.com>
2020-04-24 16:50:38 +0300

    Added bunch of features, including the ability to auto-translate using a google API

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,24 @@
 import * as vscode from 'vscode';
 
-import { generateTranslationString } from './lib';
+import { generateTranslationString, generateTranslationStringCodeOnly, generateStringOnly } from './lib';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'extension.generateTranslationString',
     generateTranslationString,
   );
+  let disposable2 = vscode.commands.registerCommand(
+    'extension.generateTranslationStringCodeOnly',
+    generateTranslationStringCodeOnly,
+  );
+  let disposable3 = vscode.commands.registerCommand(
+    'extension.generateStringOnly',
+    generateStringOnly,
+  );
   context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable2);
+  context.subscriptions.push(disposable3);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
